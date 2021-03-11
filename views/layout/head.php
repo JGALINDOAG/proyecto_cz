0afe7ba6593666f4654bb1412b040b645db0155e
Erick Badillo <erickbadillo@Ericks-MacBook-Pro.local>
2021-03-11 17:08:19 -0600

    hope

--- views/layout/head.php
+++ views/layout/head.php
@@ -2,9 +2,6 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <!-- Jquery -->
-    <script type="text/javascript" language="javascript" src="<?php echo AccesoDatos::ruta(); ?>assets/js/jquery-3.5.1.min.js"></script>
-
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="<?php echo AccesoDatos::ruta(); ?>libs/bootstrap-4/css/bootstrap.css">
     <!-- DataTable -->
@@ -15,14 +12,5 @@
     <link rel="stylesheet" href="<?php echo AccesoDatos::ruta(); ?>assets/css/styleBtp.css">
 
     <link rel="icon" type="image/svg" sizes="192x192" href="<?php echo AccesoDatos::ruta(); ?>assets/img/ico.png">
-    <!-- RAVEN -->
-    <style>
-    .group-item img {
-        display: block;
-        margin-left: auto;
-        margin-right: auto;
-        width: 35%;
-    }
-    </style>
     <title>Cruzeac | Cruzeac Consultores S. A. de C. V.</title>
 </head>
