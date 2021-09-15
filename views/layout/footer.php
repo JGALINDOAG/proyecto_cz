b4d60b1ea324f79b383296271f7ff463cefdd9fd
JhonatanGA <jhonatan.galindo@prepalinea.sep.gob.mx>
2021-09-15 12:50:56 -0500

    Cambio footer

--- views/layout/footer.php
+++ views/layout/footer.php
@@ -2,16 +2,6 @@
     <footer>
         <div class="card text-center">
             <div class="card-header">&nbsp;</div>
-            <div class="card-body">
-                <h5 class="card-title text-muted">Ajoloapan núm. 18 mz. 9 lt. 19. Tecámac Centro, Estado México</h5>
-                <p class="text-muted">Email: chromeconsultores@outlook.com</p>
-                <p class="text-muted">Tel. Oficina (55) 8857 1239</p>
-                <p class="text-muted">Cel. 55 3388 1247 (Solo mensajes)</p>
-                <p class="text-muted">Cel. 55 3388 1247 (Solo mensajes)</p>
-            </div>
-            <!-- <div class="card-footer text-muted">
-                <?php echo strftime("%d de %B del %Y %r", strtotime(date('Y-m-d G:i:s'))); ?>
-            </div> -->
         </div>
     </footer>
 </section>
