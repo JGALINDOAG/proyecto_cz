6717d680e1dba190eb83a576873b53b8f678645b
JhonatanGA <jhonatan.galindo@prepalinea.sep.gob.mx>
2021-10-04 12:42:54 -0500

    Se agregaron librerias

--- views/layout/head.php
+++ views/layout/head.php
@@ -6,11 +6,11 @@
     <link rel="stylesheet" href="<?php echo AccesoDatos::ruta(); ?>libs/bootstrap-4/css/bootstrap.css">
     <!-- DataTable -->
     <link rel="stylesheet" href="<?php echo AccesoDatos::ruta(); ?>libs/DataTables/css/dataTables.bootstrap4.css">
-    <link rel="stylesheet" href="https://cdn.datatables.net/searchpanes/1.2.1/css/searchPanes.dataTables.min.css">
-    <link rel="stylesheet" href="https://cdn.datatables.net/select/1.3.2/css/select.dataTables.min.css">
-    <link rel="stylesheet" href="https://cdn.datatables.net/buttons/2.0.0/css/buttons.dataTables.min.css">
+    <link rel="stylesheet" href="<?php echo AccesoDatos::ruta(); ?>libs/DataTables/css/searchPanes.dataTables.min.css">
+    <link rel="stylesheet" href="<?php echo AccesoDatos::ruta(); ?>libs/DataTables/css/select.dataTables.min.css">
+    <link rel="stylesheet" href="<?php echo AccesoDatos::ruta(); ?>libs/DataTables/css/buttons.dataTables.min.css">
     <!-- bootstrap-select -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.13.9/css/bootstrap-select.min.css" />
+    <link rel="stylesheet" href="<?php echo AccesoDatos::ruta(); ?>libs/bootstrap-select/css/bootstrap-select.css" />
     <!-- CSS -->
     <link rel="stylesheet" href="<?php echo AccesoDatos::ruta(); ?>assets/css/styleBtp.css">
     <link rel="icon" type="image/svg" sizes="192x192" href="<?php echo AccesoDatos::ruta(); ?>assets/img/ico.png">
@@ -27,15 +27,15 @@
         }
     </style>
     <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
-    <script src="<?php echo AccesoDatos::ruta(); ?>assets/js/jquery-3.5.1.js"></script>
+    <script src="<?php echo AccesoDatos::ruta(); ?>libs/jQuery/jquery-3.5.1.js"></script>
     <script src="<?php echo AccesoDatos::ruta(); ?>libs/bootstrap-4/js/bootstrap.bundle.min.js"></script>
     <!-- DataTable -->
     <script src="<?php echo AccesoDatos::ruta(); ?>libs/DataTables/js/jquery.dataTables.min.js"></script>
     <script src="<?php echo AccesoDatos::ruta(); ?>libs/DataTables/js/dataTables.bootstrap4.min.js"></script>
-    <script src="https://cdn.datatables.net/searchpanes/1.2.1/js/dataTables.searchPanes.min.js"></script>
-    <script src="https://cdn.datatables.net/select/1.3.2/js/dataTables.select.min.js"></script>
+    <script src="<?php echo AccesoDatos::ruta(); ?>libs/DataTables/js/dataTables.searchPanes.min.js"></script>
+    <script src="<?php echo AccesoDatos::ruta(); ?>libs/DataTables/js/dataTables.select.min.js"></script>
     <!-- Bootstrap-select -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.13.18/js/bootstrap-select.min.js"></script>
+    <script src="<?php echo AccesoDatos::ruta(); ?>libs/bootstrap-select/js/bootstrap-select.min.js"></script>
     <!-- FUNCIONES -->
     <script src="<?php echo AccesoDatos::ruta(); ?>assets/js/function.js"></script>
     <title>Chrome | S. A. de C. V.</title>
